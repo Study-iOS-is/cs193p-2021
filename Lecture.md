d5d4362b0dcfc0f95122c6d83448b350327350b9
sei <vanism7557@gmail.com>
2023-02-08 18:30:43 +0900

    Study: Lecture2-2. Custom View Type with Stored Property

--- Lecture.md
+++ Lecture.md
@@ -3,7 +3,18 @@
 - opaque return type
 ```swift
 Text("Hello world!"): Text
+// Error occured!
 Text("hi").foregroundColor(.red): some View 
-// Error:
 // Property definition has inferred type 'some View', involving the 'some' return type of another declaration
 ```
+- padding
+```swift
+func padding(_ edges: Edge.Set = .all, _ length: CGFloat? = nil) -> some View
+```
+
+# 2
+
+- custom view type
+- custom view type에 stored property 추가
+- ZStack 안에 local variable을 선언할 수도 있다.
+  - 강의와 달리 var여도 별 다른 노란 경고가 발생하지는 않음!
--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
+            CardView(isFaceUp: true)
             CardView()
-            CardView()
-            CardView()
+            CardView(isFaceUp: true)
             CardView()
         }
         .padding([.top, .bottom])
@@ -21,13 +21,17 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    var isFaceUp: Bool = false
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("🍎")
-                .foregroundColor(.red)
-                .font(.largeTitle)
+            var shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill(.white)
+                shape.stroke(lineWidth: 3)
+                Text("🍎").font(.largeTitle)
+            } else {
+                shape.fill()
+            }
         }
     }
 }
@@ -36,5 +40,8 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
